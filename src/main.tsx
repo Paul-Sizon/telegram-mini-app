c097eb37c130fa2d7361f24fcaa2893986a2d403
paul.sizon <paul.sizon@outlook.com>
2024-07-27 14:04:16 -0300

    manifest

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 import WebApp from '@twa-dev/sdk'
 
@@ -9,6 +10,8 @@ WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TonConnectUIProvider manifestUrl="https://paul-sizon.github.io/tma/tonconnect-manifest.json">
+      <App />
+    </TonConnectUIProvider>
   </React.StrictMode>,
 )
